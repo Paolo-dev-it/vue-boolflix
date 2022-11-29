297b4688e36dc380d0327141448eaa0afc5a6965
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-11-29 15:31:46 +0100

    Variables films and series completed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <HeaderComp />
+    <HeaderComp @nomeEmit="getFilms" />
     <MainComp />
   </div>
 </template>
@@ -10,13 +10,44 @@ import HeaderComp from "./components/HeaderComp.vue";
 import MainComp from "./components/MainComp.vue";
 
 /* Importo Axios */
-//import axios from "axios";
+import axios from "axios";
 
 export default {
   name: "App",
   components: {
     HeaderComp,
     MainComp,
+  },
+  data() {
+    return {
+      dataArrayFilms: [],
+      dataArraySeries: [],
+      keyApi: "9346788ddcf5deaf90fa51b45fc1d1fc",
+    };
+  },
+
+  mounted() {
+    this.getFilms();
+  },
+
+  methods: {
+    getFilms(testoInput) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=${this.keyApi}&query=${testoInput}`
+        )
+        .then((response) => {
+          this.dataArrayFilms = response.data.results;
+        });
+
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/tv?api_key=${this.keyApi}&query=${testoInput}`
+        )
+        .then((response) => {
+          this.dataArraySeries = response.data.results;
+        });
+    },
   },
 };
 </script>
--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="hello"></div>
+  <div class="container">
+    <input
+      type="text"
+      placeholder="Search"
+      v-model="testoInput"
+      @keyup.enter="$emit('nomeEmit', testoInput)"
+    />
+    <button @click="$emit('nomeEmit', testoInput)">Search</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HeaderComp",
   props: {},
+  data() {
+    return {
+      testoInput: "",
+    };
+  },
+
+  methods: {},
 };
 </script>
 
