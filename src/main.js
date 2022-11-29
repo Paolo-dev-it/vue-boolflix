31a9f89768dac4c057fc9824eadf7ab7753824f2
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-11-29 11:00:44 +0100

    Installed vue 2, axios and font-awesome

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUserSecret);
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
